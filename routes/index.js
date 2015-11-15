b0e8961320fad5bcf39ecf856bddebd3e0e62668
Rina Ahmed <rinaa@microsoft.com>
2015-11-15 18:43:18 +0100

    v3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My site v2' });
+  res.render('index', { title: 'My site v3' });
 });
 
 module.exports = router;
