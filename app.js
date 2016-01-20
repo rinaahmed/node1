c0f927df76d7da90f870916e3f09b97700977c51
Rina Ahmed <rina.ahmed@live.com>
2016-01-20 12:12:09 +0100

    changed title

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use('/users', users);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
+  next(err);   
+  
 });
 
 // error handlers
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome, Ready2Order' });
+  res.render('index', { title: 'Welcome, Ready' });
 });
 
 module.exports = router;
