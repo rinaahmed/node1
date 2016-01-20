3a77c300d6cea26a8aff08b0850456cb49a7101e
Rina Ahmed <rina.ahmed@live.com>
2016-01-20 17:10:00 +0100

    Changed Technikum Title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to our Pizzatour' });
+  res.render('index', { title: 'Welcome to Technikum Wien' });
 });
 
 module.exports = router;
