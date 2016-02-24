0efa4ff5e535a4081203a6b26bc5adfd777fc2d3
Rina Ahmed <rina.ahmed@live.com>
2016-02-24 17:23:06 +0100

    Changing Title to Graz

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Pizza Tour' });
+  res.render('index', { title: 'Welcome to Pizza Tour Graz' });
 });
+
 
 module.exports = router;
