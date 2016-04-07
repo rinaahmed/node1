69e4b589494133e7600d0396f8d5794992ca55d8
Rina Ahmed <rina.ahmed@live.com>
2016-04-07 16:05:52 +0200

    change in title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Pizza Tour' });
+  res.render('index', { title: 'Welcome to Pizza Tour in JKU' });
 });
 
 
